d1ad89dcb82d787a19f2df914bb1fc378cf40c7f
alejandracapis <acapistran17@gmail.com>
2024-11-08 13:25:32 -0600

    .

--- src/index.js
+++ src/index.js
@@ -10,11 +10,9 @@ import '@fontsource/karla';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
